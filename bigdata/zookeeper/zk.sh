8dee97b4a6a69f8ce271fb3a2392920a4c739b5a
honeok <yihaohey@gmail.com>
2025-05-19 01:55:58 +0800

    chore: modify variable name

--- bigdata/zookeeper/zk.sh
+++ bigdata/zookeeper/zk.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-hosts=(kafka01 kafka02 kafka03)
+HOSTS=(kafka01 kafka02 kafka03)
 
 case "$1" in
     'start')
-        for host in "${hosts[@]}"; do
+        for host in "${HOSTS[@]}"; do
             echo "------------- zookeeper $host starting ------------"
             ssh "$host" "/opt/module/zookeeper-3.5.7/bin/zkServer.sh start"
         done
     ;;
     'stop')
-        for host in "${hosts[@]}"; do
+        for host in "${HOSTS[@]}"; do
             echo "------------- zookeeper $host stopping ------------"
             ssh "$host" "/opt/module/zookeeper-3.5.7/bin/zkServer.sh stop"
         done
     ;;
     'status')
-        for host in "${hosts[@]}"; do
+        for host in "${HOSTS[@]}"; do
             echo "------------- zookeeper $host status ------------"
             ssh "$host" "/opt/module/zookeeper-3.5.7/bin/zkServer.sh status"
         done
