a1ef72179bc81503d04e8e77c9a7dcd1d4a8aa8b
honeok <yihaohey@gmail.com>
2025-05-23 15:40:11 +0800

    feat: use daocloud as a domestic proxy to download

--- script/go.sh
+++ script/go.sh
@@ -17,6 +17,10 @@ _err_msg() { printf "\033[41m\033[1mError\033[0m %s\n" "$*"; }
 _suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %s\n" "$*"; }
 _info_msg() { printf "\033[43m\033[1mInfo\033[0m %s\n" "$*"; }
 
+# 各变量默认值
+GITHUB_PROXY='https://files.m.daocloud.io/'
+UA_BROWSER='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+
 _is_exists() {
     local _CMD="$1"
     if type "$_CMD" >/dev/null 2>&1; then return 0;
@@ -39,6 +43,10 @@ pre_check() {
     fi
     if [ "$(ps -p $$ -o comm=)" != "bash" ] || readlink /proc/$$/exe | grep -q "dash"; then
         error_and_exit "This script needs to be run with bash, not sh!"
+    fi
+    # 境外服务器仅ipv4访问测试通过后取消github代理
+    if [ "$(curl --user-agent "$UA_BROWSER" -fsL -m 3 --retry 2 -4 "http://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^loc=' | cut -d'=' -f2 | grep .)" != "CN" ]; then
+        unset GITHUB_PROXY
     fi
 }
 
@@ -66,7 +74,7 @@ install_go() {
         ;;
     esac
     _info_msg "$(_yellow "Start downloading the go install package.")"
-    if ! curl -L -O "https://go.dev/dl/go$GO_LVER.linux-$GO_FRAMEWORK.tar.gz"; then
+    if ! curl -L -O "${GITHUB_PROXY}go.dev/dl/go$GO_LVER.linux-$GO_FRAMEWORK.tar.gz"; then
         error_and_exit "Failed to download go install package, please check the network!"
     fi
     rm -rf "$GO_WORKDIR" >/dev/null 2>&1
