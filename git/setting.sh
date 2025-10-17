a6825e06a8f364e5534f38f039a421ceb2651860
honeok <i@honeok.com>
2025-10-17 14:52:33 +0800

    feat: git basic operating environment config
    Signed-off-by: honeok <i@honeok.com>

--- git/setting.sh
+++ git/setting.sh
@@ -1,23 +1,27 @@
-#!/bin/bash
+#!/bin/bash -eEu
+# SPDX-License-Identifier: MIT
 #
-# Description: This script is used to set the upstream repository commit user.
+# Description: This script is used to set up the git basic operating environment.
 #
 # Copyright (c) 2025 honeok <i@honeok.com>
-#
-# SPDX-License-Identifier: MIT
 
-set -e
-
-REPOSITORY="$(pwd | awk -F'/' '{print $NF}')"
-CODE_PLATFORM="$(pwd | awk -F'/' '{print $(NF-1)}')"
+PLATFORM="$(pwd | awk -F'/' '{print $(NF-1)}')"
+REMOTE_ORIGIN="$(git config --get remote.origin.url)"
 separator() { printf "%-20s\n" "-" | sed 's/\s/-/g'; }
 
-case "$CODE_PLATFORM" in
+case "$PLATFORM" in
     github*) WORK_PLATFORM="github" ;;
     gitlab*) WORK_PLATFORM="gitlab" ;;
-    *) echo "Error: Unknown platform."; exit 1 ;;
+    *) echo "Error: Unknown platform." && exit 1 ;;
 esac
 
+# global variable
+if ! git config --global --get-regexp url | grep -Fx "url.ssh://git@ssh.github.com:443/.insteadof git@github.com:" >/dev/null 2>&1; then
+    git config --global url."ssh://git@ssh.github.com:443/".insteadof git@github.com:
+    ssh -T -p 443 git@ssh.github.com
+fi
+
+# set user
 while true; do
     separator
     echo " 1. honeok"
@@ -25,22 +29,24 @@ while true; do
     separator
     read -rep "Please enter user: " USER
     case "$USER" in
-        1 )
+        1)
             git config user.name honeok
             [ "$WORK_PLATFORM" = "github" ] && git config user.email "100125733+honeok@users.noreply.github.com"
-            git remote set-url origin "git@${WORK_PLATFORM}-honeok:honeok/${REPOSITORY}.git"
+            git remote set-url origin "$REMOTE_ORIGIN"
             break
         ;;
-        2 )
+        2)
             git config user.name havario
             [ "$WORK_PLATFORM" = "github" ] && git config user.email "157877551+havario@users.noreply.github.com"
-            git remote set-url origin "git@${WORK_PLATFORM}-havario:havario/${REPOSITORY}.git"
+            git remote set-url origin "$REMOTE_ORIGIN"
             break
         ;;
-        * )
+        *)
             echo "Error: Unknown User"
         ;;
     esac
 done
+
+separator
 git config --get user.name
 git config --get user.email
