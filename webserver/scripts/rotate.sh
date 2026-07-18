a1aa39dc2773ee115cc17bb42eb2a10744454ae0
honeok <i@honeok.com>
2026-07-19 01:52:45 +0800

    fix: improve portability and location reporting
    Add script versioning, normalize local variable names, and simplify
    command detection and package installation across supported systems.
    Improve curl failure handling, add freenginx container detection, and
    use a configurable default Bark endpoint.
    Detect the active IP version and report the server city and country
    through the matching IPinfo endpoint.
    
    Signed-off-by: honeok <i@honeok.com>

--- webserver/scripts/rotate.sh
+++ webserver/scripts/rotate.sh
@@ -2,15 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Description: This script is used to rotate web server logs and push message to tgbot and bark server.
-# Copyright (c) 2025 honeok <i@honeok.com>
+# Copyright (c) 2025-2026 honeok <i@honeok.com>
 
 set -eEuo pipefail
+
+# shellcheck disable=SC2034
+readonly SCRIPT_VERSION='0.2.0'
 
 # 各变量默认值
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 BOT_TOKEN=""
 CHAT_ID=""
-BARK_URL=""
+: "${BARK_URL:="api.day.app"}"
 BARK_TOKEN=""
 
 # 设置PATH环境变量
@@ -19,52 +22,52 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 export DEBIAN_FRONTEND=noninteractive
 
 die() {
-    echo >&2 "Error: $*"
+    echo "Error: $*" >&2
     exit 1
 }
 
-_exists() {
-    local _CMD="$1"
-    if type "$_CMD" > /dev/null 2>&1; then
-        return
-    elif command -v "$_CMD" > /dev/null 2>&1; then
-        return
-    elif which "$_CMD" > /dev/null 2>&1; then
-        return
-    else
-        return 1
-    fi
-}
-
 curl() {
-    local RET
+    local rc
+
+    # 添加 -f --fail 不然 404 退出码也为 0
+    # 32位 cygwin 已停止更新 证书可能有问题 添加 --insecure
+    # centos7 curl 不支持 --retry-connrefused --retry-all-errors 因此手动 retry
     for ((i = 1; i <= 3; i++)); do
-        command curl --connect-timeout 10 --fail --insecure "$@"
-        RET="$?"
-        if [ "$RET" -eq 0 ]; then
+        if command curl --connect-timeout 10 --fail --insecure "$@"; then
             return
         else
-            if [ "$RET" -eq 22 ] || [ "$i" -eq 5 ]; then
-                return "$RET"
+            rc="$?"
+            # 403 404 错误 或达到重试次数
+            if [ "$rc" -eq 22 ] || [ "$i" -eq 5 ]; then
+                return "$rc"
             fi
             sleep 1
         fi
     done
 }
 
-pkg_install() {
+get_cmd_path() {
+    # arch 云镜像不带 which
+    # command -v 包括脚本里面的方法
+    # ash 无效
+    type -f -p "$1"
+}
+
+is_have_cmd() {
+    get_cmd_path "$1" > /dev/null 2>&1
+}
+
+install_pkg() {
     for pkg in "$@"; do
-        if _exists dnf; then
+        if is_have_cmd dnf; then
             dnf install -y "$pkg"
-        elif _exists yum; then
+        elif is_have_cmd yum; then
             yum install -y "$pkg"
-        elif _exists apt-get; then
+        elif is_have_cmd apt-get; then
             apt-get update
             apt-get install -y -q "$pkg"
-        elif _exists apk; then
+        elif is_have_cmd apk; then
             apk add --no-cache "$pkg"
-        elif _exists pacman; then
-            pacman -S --noconfirm --needed "$pkg"
         else
             die "The package manager is not supported."
         fi
@@ -72,7 +75,7 @@ pkg_install() {
 }
 
 check_root() {
-    if [ "$EUID" -ne 0 ] || [ "$(id -ru)" -ne 0 ]; then
+    if [ "$EUID" -ne 0 ]; then
         die "This script must be run as root."
     fi
 }
@@ -82,18 +85,20 @@ check_cmd() {
     INSTALL_PKG=("curl" "gzip")
 
     for pkg in "${INSTALL_PKG[@]}"; do
-        if ! _exists "$pkg" > /dev/null 2>&1; then
-            pkg_install "$pkg"
+        if ! is_have_cmd "$pkg" > /dev/null 2>&1; then
+            install_pkg "$pkg"
         fi
     done
 }
 
 check_srv() {
-    local -a WEB_SRV
-    WEB_SRV=("nginx" "openresty" "tengine")
-    for ((i = 0; i < ${#WEB_SRV[@]}; i++)); do
-        if docker ps --filter "name=${WEB_SRV[i]}" -q | grep -q .; then
-            CONTAINER_NAME="${WEB_SRV[i]}"
+    local -a web_srv
+
+    web_srv=("freenginx" "nginx" "openresty" "tengine")
+
+    for ((i = 0; i < ${#web_srv[@]}; i++)); do
+        if docker ps --filter "name=${web_srv[i]}" -q | grep -q .; then
+            CONTAINER_NAME="${web_srv[i]}"
             break
         fi
     done
@@ -105,47 +110,54 @@ check_srv() {
 
 # 日志截断
 log_rotate() {
-    local START_TIME
-    START_TIME="$(date +%Y-%m-%d-%S)"
+    local start_time
+
+    start_time="$(date -u '+%Y-%m-%d-%S' -d '+8 hours')"
 
     cd "${SCRIPT_DIR:?}" > /dev/null 2>&1 || die "Cannot enter directory."
-    mv -f logs/access.log "logs/access_$START_TIME.log" > /dev/null 2>&1
-    mv -f logs/error.log "logs/error_$START_TIME.log" > /dev/null 2>&1
+    mv -f logs/access.log "logs/access_$start_time.log" > /dev/null 2>&1
+    mv -f logs/error.log "logs/error_$start_time.log" > /dev/null 2>&1
     docker exec "$CONTAINER_NAME" nginx -s reopen > /dev/null 2>&1
-    gzip "logs/access_$START_TIME.log" > /dev/null 2>&1
-    gzip "logs/error_$START_TIME.log" > /dev/null 2>&1
+    gzip "logs/access_$start_time.log" > /dev/null 2>&1
+    gzip "logs/error_$start_time.log" > /dev/null 2>&1
     find logs -type f -name "*.log.gz" -mtime +7 -exec rm -f {} \; > /dev/null 2>&1
 }
 
 send_msg() {
-    local MESSAGE="$1"
+    local msg
+
+    msg="$1"
 
     if [[ -n "$BOT_TOKEN" && -n "$CHAT_ID" ]]; then
         curl -Ls -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
             -H "Content-Type: application/json" \
-            -d "{\"chat_id\":\"$CHAT_ID\",\"text\":\"$MESSAGE\"}" > /dev/null 2>&1 || true
+            -d "{\"chat_id\":\"$CHAT_ID\",\"text\":\"$msg\"}" > /dev/null 2>&1 || true
     fi
+
     if [ -n "$BARK_TOKEN" ]; then
-        curl -Ls -X POST "https://${BARK_URL:-api.day.app}/$BARK_TOKEN" \
+        curl -Ls -X POST "https://$BARK_URL/$BARK_TOKEN" \
             -H "Content-Type: application/json" \
-            -d "{\"title\":\"$CONTAINER_NAME\",\"body\":\"${MESSAGE//$'\n'/\\n}\"}" > /dev/null 2>&1 || true
+            -d "{\"title\":\"$CONTAINER_NAME\",\"body\":\"${msg//$'\n'/\\n}\"}" > /dev/null 2>&1 || true
     fi
 }
 
 ip_address() {
-    local IPV4_ADDRESS IPV6_ADDRESS
+    local ipv4_addr ipv6_addr
 
-    IPV4_ADDRESS="$(curl -Ls -4 http://www.qualcomm.cn/cdn-cgi/trace 2> /dev/null | grep -i '^ip=' | cut -d'=' -f2 || true)"
-    IPV6_ADDRESS="$(curl -Ls -6 http://www.qualcomm.cn/cdn-cgi/trace 2> /dev/null | grep -i '^ip=' | cut -d'=' -f2 || true)"
+    ipv4_addr="$(curl -Ls -4 http://www.qualcomm.cn/cdn-cgi/trace 2> /dev/null | grep -i '^ip=' | cut -d'=' -f2 || true)"
+    ipv6_addr="$(curl -Ls -6 http://www.qualcomm.cn/cdn-cgi/trace 2> /dev/null | grep -i '^ip=' | cut -d'=' -f2 || true)"
 
-    if [[ -n "$IPV4_ADDRESS" && "$IPV4_ADDRESS" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        PUBLIC_IP="$IPV4_ADDRESS"
-        MASKED_IP="$(awk -F. 'NF==4{print $1"."$2".*.*"} NF!=4{print ""}' <<< "$IPV4_ADDRESS")"
+    if [[ -n "$ipv4_addr" && "$ipv4_addr" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        PUBLIC_IP="$ipv4_addr"
+        MASKED_IP="$(awk -F. 'NF==4{print $1"."$2".*.*"} NF!=4{print ""}' <<< "$ipv4_addr")"
+        IP_VER='4'
         return
     fi
-    if [[ -n "$IPV6_ADDRESS" && "$IPV6_ADDRESS" == *":"* ]]; then
-        PUBLIC_IP="[$IPV6_ADDRESS]"
-        MASKED_IP="$(awk -F: '{print $1":"$2":"$3":*:*:*:*:*"}' <<< "$IPV6_ADDRESS")"
+
+    if [[ -n "$ipv6_addr" && "$ipv6_addr" == *":"* ]]; then
+        PUBLIC_IP="[$ipv6_addr]"
+        MASKED_IP="$(awk -F: '{print $1":"$2":"$3":*:*:*:*:*"}' <<< "$ipv6_addr")"
+        IP_VER='6'
         return
     fi
 
@@ -153,22 +165,29 @@ ip_address() {
 }
 
 ip_info() {
-    local IP_API
+    local ipinfo_url ipinfo_result
 
-    IP_API="$(curl -Ls "https://api.ipbase.com/v1/json/$PUBLIC_IP")"
-    SERVER_CITY="$(sed -En 's/.*"(city_name|cityName|city)":[ ]*"([^"]+)".*/\2/p' <<< "$IP_API")"
+    case "$IP_VER" in
+    4) ipinfo_url="https://ipinfo.io/$PUBLIC_IP/json" ;;
+    6) ipinfo_url="https://v6.ipinfo.io/$PUBLIC_IP/json" ;;
+    *) return 1 ;;
+    esac
+
+    ipinfo_result="$(curl -Ls -"$IP_VER" "$ipinfo_url")" || return
+    SERVER_LOCATION="$(awk -F'"' '$2=="city"{city=$4} $2=="country"{country=$4} END{print city (city&&country?", ":"") country}' <<< "$ipinfo_result")"
 }
 
 # 构建消息推送
 const_msg() {
-    local END_TIME
-    END_TIME="$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours')"
+    local end_time
+
+    end_time="$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours')"
 
     ip_address
     ip_info
 
-    send_msg "$END_TIME
-$MASKED_IP $SERVER_CITY
+    send_msg "$end_time
+$MASKED_IP $SERVER_LOCATION
 $CONTAINER_NAME complete log rotation!"
 }
 
